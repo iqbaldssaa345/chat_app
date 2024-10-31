9a7d6aa81b920c6d039d5f05088679b62eb26c93
iqbaldssaa345 <muhiqbaldwi05@gmail.com>
2024-10-31 10:31:36 +0700

    iqbaldua

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-halo uji coba koding<code></code>
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php 
+if (!isset($_SESSION['iduser'])) {
+    include"login.php";
+}else{
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampilan Chat room</title>
+</head>
+<body>
+    <h1>Room Chat</h1>
+</body>
+</html>
+<?php } ?>
